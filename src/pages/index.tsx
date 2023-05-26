c08a9884cc292f22a199ced3b3a8845785173c31
Bjørn Are Nielsen <bjornarenielsen@yahoo.no>
2023-05-26 09:51:25 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,217 +1,88 @@
-// import { remult, UserInfo } from "remult";
-// import { signIn, signOut, useSession } from "next-auth/react";
-// import { FormEvent, useEffect, useState } from "react";
-// import { Task } from "../shared/Task";
-
-// const taskRepo = remult.repo(Task);
-
-// export default function Home() {
-//   const [tasks, setTasks] = useState<Task[]>([]);
-//   const [newTaskTitle, setNewTaskTitle] = useState("");
-//   const session = useSession();
-
-//   const addTask = async (e: FormEvent) => {
-//     e.preventDefault();
-//     try {
-//       const newTask = await taskRepo.insert({ title: newTaskTitle });
-//       setTasks([...tasks, newTask]);
-//       setNewTaskTitle("");
-//     } catch (error: any) {
-//       alert(error.message);
-//     }
-//   };
-
-//   useEffect(() => {
-//     remult.user = session.data?.user as UserInfo;
-//     if (session.status === "unauthenticated") signIn();
-//     else if (session.status === "authenticated")
-//       return taskRepo
-//         .liveQuery({
-//           limit: 20,
-//           orderBy: { completed: "asc" },
-//         })
-//         .subscribe((info) => setTasks(info.applyChanges));
-//   }, [session]);
-//   if (session.status !== "authenticated") return <></>;
-//   return (
-//     <div>
-//       <h1>Todos</h1>
-//       <div>
-//         Hello {remult.user?.name}
-//         <button onClick={() => signOut()}>Sign Out</button>
-//       </div>
-//       <main>
-//         {taskRepo.metadata.apiInsertAllowed() && (
-//           <form onSubmit={addTask}>
-//             <input
-//               value={newTaskTitle}
-//               placeholder="What needs to be done?"
-//               onChange={(e) => setNewTaskTitle(e.target.value)}
-//             />
-//             <button>Add</button>
-//           </form>
-//         )}
-
-//         {tasks.map((task) => {
-//           const setTask = (value: Task) =>
-//             setTasks((tasks) => tasks.map((t) => (t === task ? value : t)));
-
-//           const setTitle = (title: string) => setTask({ ...task, title });
-
-//           const saveTask = async () => {
-//             try {
-//               await taskRepo.save(task);
-//             } catch (error: any) {
-//               alert(error.message);
-//             }
-//           };
-
-//           const deleteTask = async () => {
-//             try {
-//               await taskRepo.delete(task);
-//             } catch (error: any) {
-//               alert(error.message);
-//             }
-//           };
-
-//           return (
-//             <div key={task.id}>
-//               <input
-//                 value={task.title}
-//                 onChange={(e) => setTitle(e.target.value)}
-//               />
-//               <button onClick={saveTask}>Save</button>
-//               {taskRepo.metadata.apiDeleteAllowed(task) && (
-//                 <button onClick={deleteTask}>Delete</button>
-//               )}
-//             </div>
-//           );
-//         })}
-//       </main>
-//     </div>
-//   );
-// }
 import { remult, UserInfo } from "remult";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { FormEvent, useEffect, useState } from "react";
 import { Task } from "../shared/Task";
-import ably from "ably/promises";
-import { AblySubscriptionClient } from "remult/ably";
-import { InferGetServerSidePropsType } from "next";
-import remultApi from "./api/[...remult]";
 
 const taskRepo = remult.repo(Task);
 
-export const getServerSideProps = remultApi.getServerSideProps(async (req) => {
-  return {
-    props: {
-      tasks: taskRepo.metadata.apiReadAllowed ? await fetchTasks() : [],
-    },
-  };
-});
+export default function Home() {
+const [tasks, setTasks] = useState<Task[]>([]);
+const [newTaskTitle, setNewTaskTitle] = useState("");
+const session = useSession();
 
-export default function Home(
-  props: InferGetServerSidePropsType<typeof getServerSideProps>
-) {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState("");
-  const session = useSession();
+const addTask = async (e: FormEvent) => {
+e.preventDefault();
+     try {
+       const newTask = await taskRepo.insert({ title: newTaskTitle });
+       setTasks([...tasks, newTask]);
+       setNewTaskTitle("");
+     } catch (error: any) {
+       alert(error.message);
+     }
+   };
 
-  const addTask = async (e: FormEvent) => {
-    e.preventDefault();
-    try {
-      const newTask = await taskRepo.insert({ title: newTaskTitle });
-      setTasks([...tasks, newTask]);
-      setNewTaskTitle("");
-    } catch (error: any) {
-      alert(error.message);
-    }
-  };
-
-  const setTitle = (task: Task, title: string) => {
-    const updatedTasks = tasks.map((t) => {
-      if (t === task) {
-        return { ...t, title: title };
-      }
-      return t;
-    });
-    setTasks(updatedTasks);
-  };
-
-  const saveTask = async (task: Task) => {
-    try {
-      await taskRepo.update(task.id, { ...task });
-    } catch (error: any) {
-      alert(error.message);
-    }
-  };
-
-  useEffect(() => {
-    remult.apiClient.subscriptionClient = new AblySubscriptionClient(
-      new ably.Realtime({ authUrl: "/api/getAblyToken" })
-    );
-  }, []);
-
-  useEffect(() => {
-    remult.user = session.data?.user as UserInfo;
-    if (session.status === "unauthenticated") signIn();
-    else if (session.status === "authenticated")
-      return taskRepo
-        .liveQuery({
-          limit: 20,
-          orderBy: { completed: "asc" },
-          // where: { completed: true },
-        })
-        .subscribe((info) => setTasks(info.applyChanges));
-  }, [session]);
-
-  if (session.status !== "authenticated") return <></>;
-
-  return (
-    <div>
-      <h1>Todos</h1>
-      <div>
-        Hello {remult.user?.name}
-        <button onClick={() => signOut()}>Sign Out</button>
-      </div>
-      <main>
-        {taskRepo.metadata.apiInsertAllowed() && (
-          <form onSubmit={addTask}>
-            <input
-              value={newTaskTitle}
-              placeholder="What needs to be done?"
-              onChange={(e) => setNewTaskTitle(e.target.value)}
-            />
-            <button>Add</button>
-          </form>
-        )}
-
-        {tasks.map((task) => {
-          const deleteTask = async () => {
-            try {
-              await taskRepo.delete(task);
-            } catch (error: any) {
-              alert(error.message);
-            }
-          };
-
-          return (
-            <div key={task.id}>
-              <input
-                value={task.title}
-                onChange={(e) => setTitle(task, e.target.value)}
-              />
-              <button onClick={() => saveTask(task)}>Save</button>
-              {taskRepo.metadata.apiDeleteAllowed(task) && (
-                <button onClick={deleteTask}>Delete</button>
-              )}
-            </div>
-          );
-        })}
-      </main>
-    </div>
-  );
-}
-function fetchTasks(): any {
-  throw new Error("Function not implemented.");
-}
+useEffect(() => {
+     remult.user = session.data?.user as UserInfo;
+     if (session.status === "unauthenticated") signIn();
+     else if (session.status === "authenticated")
+       return taskRepo
+         .liveQuery({
+           limit: 20,
+           orderBy: { completed: "asc" },
+         })
+         .subscribe((info) => setTasks(info.applyChanges));
+   }, [session]);
+   if (session.status !== "authenticated") return <></>;
+   return (
+     <div>
+       <h1>Todos</h1>
+       <div>
+         Hello {remult.user?.name}
+         <button onClick={() => signOut()}>Sign Out</button>
+       </div>
+       <main>
+         {taskRepo.metadata.apiInsertAllowed() && (
+           <form onSubmit={addTask}>
+             <input
+               value={newTaskTitle}
+               placeholder="What needs to be done?"
+               onChange={(e) => setNewTaskTitle(e.target.value)}
+             />
+             <button>Add</button>
+           </form>
+         )}
+         {tasks.map((task) => {
+           const setTask = (value: Task) =>
+             setTasks((tasks) => tasks.map((t) => (t === task ? value : t)));
+           const setTitle = (title: string) => setTask({ ...task, title });
+           const saveTask = async () => {
+             try {
+               await taskRepo.save(task);
+             } catch (error: any) {
+               alert(error.message);
+             }
+           };
+           const deleteTask = async () => {
+             try {
+               await taskRepo.delete(task);
+             } catch (error: any) {
+               alert(error.message);
+             }
+           };
+           return (
+             <div key={task.id}>
+               <input
+                 value={task.title}
+                 onChange={(e) => setTitle(e.target.value)}
+               />
+               <button onClick={saveTask}>Save</button>
+               {taskRepo.metadata.apiDeleteAllowed(task) && (
+                 <button onClick={deleteTask}>Delete</button>
+               )}
+             </div>
+           );
+         })}
+       </main>
+     </div>
+   );
+ }
