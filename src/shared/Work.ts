63008eab33b40d32fc39d4d0e40b90305dae43ef
bjornavonova <bjorn.nielsen@avonova.no>
2023-05-28 15:01:48 +0200

    added work

--- src/shared/Work.ts
+++ src/shared/Work.ts
@@ -1,24 +1,24 @@
 import { Allow, Entity, Fields } from "remult";
 
-@Entity("schools", {
+@Entity("works", {
   allowApiCrud: Allow.authenticated,
   allowApiInsert: "admin",
   allowApiDelete: "admin",
 })
-export class School {
+export class Work {
   @Fields.uuid()
   id!: string;
 
   @Fields.string({
-    validate: (school) => {
-      if (school.name.length < 3) throw "Too Short";
+    validate: (work) => {
+      if (work.name.length < 3) throw "Too Short";
     },
     allowApiUpdate: "admin",
   })
   name = "";
 
   @Fields.string()
-  occupation = "";
+  text = "";
 
   @Fields.date()
   fromDate?: Date;
