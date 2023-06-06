3471e31bb967ff77c07cb1f953deff1a607fce53
bare74 <bjornarenielsen@yahoo.no>
2023-06-06 15:48:37 +0200

    styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,45 @@ import Works from "@/components/Works";
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>WELCOME</h1>
-      <Link href="/edit">
-        <>Login</>
-      </Link>
-      <div>
+      <section className="banner">
+        <header>
+          <div>
+            <h2>Bjørn Are Nielsen</h2>
+            <h3>Frontend Developer</h3>
+          </div>
+          <img src="../assets/avatar.png" />
+        </header>
+        <div className="images">
+          <img height="874" src="../../src/assets/1.jpg" width="1240" />
+          <img height="874" src="2.jpg" width="1240" />
+          <img height="874" src="3.jpg" width="1240" />
+          <img height="874" src="1.jpg" width="1240" />
+        </div>
+      </section>
+
+      <main>
         <Schools />
+        <h2>Skills</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus id
+          aspernatur doloremque! Natus iure ea aliquam laborum nemo! Accusamus
+          officiis aspernatur optio asperiores modi excepturi, dicta id
+          necessitatibus nam perferendis!
+        </p>
+        <h2>Experience</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus id
+          aspernatur doloremque! Natus iure ea aliquam laborum nemo! Accusamus
+          officiis aspernatur optio asperiores modi excepturi, dicta id
+          necessitatibus nam perferendis!
+        </p>
+        <h2>Get In Touch</h2>
         <Works />
-      </div>
+
+        <Link href="/edit">
+          <>Login</>
+        </Link>
+      </main>
     </div>
   );
 };
