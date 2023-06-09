7b6e5a8d11cd9f94e4176223898e3231b7636491
bjornavonova <bjorn.nielsen@avonova.no>
2023-06-09 16:46:01 +0200

    added skills

---
+++ src/components/ComputerSkills.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { Transition } from "@headlessui/react";
+import { InView } from "react-intersection-observer";
+
+const ComputerSkills = () => {
+  const data = [
+    { id: 1, name: "Programming", occupation: "Database management" },
+
+    { id: 2, name: "Programming", occupation: "Database management" },
+    { id: 3, name: "Programming", occupation: "Database management" },
+    { id: 4, name: "Programming", occupation: "Database management" },
+    { id: 5, name: "Programming", occupation: "Database management" },
+    { id: 6, name: "Programming", occupation: "Database management" },
+  ];
+
+  return (
+    <div>
+      <h1>Schools</h1>
+      <div className="flex items-center justify-center">
+        <InView>
+          {({ inView, ref }) => (
+            <ul ref={ref} className="flex flex-wrap justify-between">
+              {data.map((item) => (
+                <li key={item.id}>
+                  <Transition.Root style={{ padding: "10px" }} show={inView}>
+                    <Transition.Child
+                      as="div"
+                      enter="ease-in duration-[1000ms] transition-all"
+                      enterFrom="opacity-0 translate-y-48"
+                      enterTo="opacity-100 translate-y-0"
+                      className="block w-[300px] h-[400px] p-6 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                      style={{
+                        backgroundColor: "rgb(64, 64, 64)",
+                        border: "1px solid rgb(130, 130, 130)",
+                      }}
+                    >
+                      <Transition.Child
+                        as="h3"
+                        enter="ease-in delay-[200ms] duration-[800ms] transition-all"
+                        enterFrom="opacity-0 translate-y-12"
+                        enterTo="opacity-100 translate-y-0"
+                        className="text-3xl font-bold"
+                      >
+                        {item.name}
+                      </Transition.Child>
+                      <Transition.Child
+                        as="div" // Change <p> to <div> here
+                        className="text-gray-300"
+                        enter="ease-in delay-[800ms] duration-[600ms] transition-all"
+                        enterFrom="opacity-0 translate-y-12"
+                        enterTo="opacity-100 translate-y-0"
+                      >
+                        <ul>
+                          <li> {item.occupation}</li>
+                          <li></li>
+                        </ul>
+                      </Transition.Child>
+                    </Transition.Child>
+                  </Transition.Root>
+                </li>
+              ))}
+            </ul>
+          )}
+        </InView>
+      </div>
+    </div>
+  );
+};
+
+export default ComputerSkills;
