62d1cbf565b703132ac27cbaef111e1d67a64a6e
bare74 <bjornarenielsen@yahoo.no>
2023-06-09 14:58:17 +0200

    Styling

--- src/pages/edit.tsx
+++ src/pages/edit.tsx
@@ -77,7 +77,17 @@ export default function Edit() {
   if (status !== "authenticated") return <></>;
 
   if (loading) {
-    return <div>Loading...</div>; // Display a loading message or spinner
+    return (
+      <div
+        className="inline-block h-12 w-12 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+        ;
+      </div>
+    );
   }
 
   return (
